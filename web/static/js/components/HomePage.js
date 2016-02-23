451bf2b1076e292c5628a1a229d1558f4f376b73
Mikkel Malmberg <mikkel@brnbw.com>
2016-02-23 13:24:23 +0100

    Add link to kitchensink

--- web/static/js/components/HomePage.js
+++ web/static/js/components/HomePage.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 
 export default class HomePage extends Component {
   render () {
     return <div>
-      Home
+      <h1>Home</h1>
+      <Link to='/kitchensink'>
+        <img src='/images/sink.png' width='85' />
+      </Link>
     </div>
   }
 }
