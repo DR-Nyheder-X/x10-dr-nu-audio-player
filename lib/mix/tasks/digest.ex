d5d34b26e7f9ab447520bd3599411abc83319f7c
Mikkel Malmberg <mikkel@brnbw.com>
2016-02-24 11:35:27 +0100

    Build with production

--- lib/mix/tasks/digest.ex
+++ lib/mix/tasks/digest.ex
@@ -2,7 +2,7 @@ defmodule Mix.Tasks.App.Digest do
   use Mix.Task
 
   def run(args) do
-    Mix.Shell.IO.cmd "./node_modules/webpack/bin/webpack.js -p"
+    Mix.Shell.IO.cmd "NODE_ENV=production ./node_modules/webpack/bin/webpack.js -p"
   end
 end
 
