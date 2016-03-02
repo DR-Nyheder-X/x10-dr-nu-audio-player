14c9d94a5e59d41ffa9fb033d77d5108ee8e32d5
Mikkel Malmberg <mikkel@brnbw.com>
2016-03-03 00:56:51 +0100

    Add fastclick, disabled user-select, add padding because statusbar

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,5 +1,8 @@
 import 'babel-polyfill'
 
+import { attach } from 'fastclick'
+attach(document.body)
+
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
